22adf02bad969dd6b6beb3de6495c570eb4c184a
zergend <zergend@gmail.com>
2023-04-04 00:26:09 +0300

    starting

---
+++ dist/js/main.js
@@ -0,0 +1,202 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.html":
+/*!********************!*\
+  !*** ./index.html ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/html-loader/dist/runtime/getUrl.js */ "../node_modules/html-loader/dist/runtime/getUrl.js");
+/* harmony import */ var _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0__);
+// Imports
+
+var ___HTML_LOADER_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ./img/logo.png */ "./img/logo.png"), __webpack_require__.b);
+// Module
+var ___HTML_LOADER_REPLACEMENT_0___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_0___);
+var code = "<!DOCTYPE html>\r\n<html lang=\"ru\">\r\n  <head>\r\n    <meta charset=\"UTF-8\" />\r\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\r\n    <title>Резиденция «Медный»</title>\r\n  </head>\r\n  <body>\r\n    <header class=\"header\">\r\n      <div class=\"container\">\r\n        <nav class=\"main-nav\">\r\n          <a class=\"logo\"\r\n            ><img\r\n              src=\"" + ___HTML_LOADER_REPLACEMENT_0___ + "\"\r\n              width=\"212\"\r\n              height=\"66\"\r\n              alt=\"Резиденция Медный\"\r\n          /></a>\r\n          <ul class=\"site-navigation\">\r\n            <li class=\"site-navigation__item\">\r\n              <a href=\"#about\" class=\"site-navigation__link\">О проекте</a>\r\n            </li>\r\n            <li class=\"site-navigation__item\">\r\n              <a href=\"#location\" class=\"site-navigation__link\">Расположение</a>\r\n            </li>\r\n            <li class=\"site-navigation__item\">\r\n              <a href=\"#select\" class=\"site-navigation__link\"\r\n                >Подобрать квартиру</a\r\n              >\r\n            </li>\r\n            <li class=\"site-navigation__item\">\r\n              <a href=\"#conditions\" class=\"site-navigation__link\"\r\n                >Условия покупки</a\r\n              >\r\n            </li>\r\n            <li class=\"site-navigation__item\">\r\n              <a href=\"#contacts\" class=\"site-navigation__link\">Контакты</a>\r\n            </li>\r\n          </ul>\r\n\r\n          <a href=\"#\" class=\"callback\">Заказать звонок</a>\r\n        </nav>\r\n      </div>\r\n    </header>\r\n\r\n    <main class=\"main\">\r\n      <h1 class=\"visually-hidden\">\r\n        Резиденция премиум-класса от «Мармакс» - «Медный»\r\n      </h1>\r\n      <section class=\"intro\">\r\n        <div class=\"container\">\r\n          <div class=\"intro__content\">\r\n            <div class=\"caption caption--intro\">\r\n              <h2 class=\"title title--light title--intro\">\r\n                Дом с уверенным характером\r\n              </h2>\r\n            </div>\r\n          </div>\r\n          <div class=\"ticker-wrap\">\r\n            <div class=\"ticker\">\r\n              <div class=\"ticker__item\">Дом премиум-класса</div>\r\n              <div class=\"ticker__item\">Пентхаусы</div>\r\n              <div class=\"ticker__item\">Потолки до 3,6 м</div>\r\n              <div class=\"ticker__item\">Центр города</div>\r\n            </div>\r\n          </div>\r\n        </div>\r\n      </section>\r\n      <section class=\"about\" id=\"about\">\r\n        <div class=\"container\">\r\n          <div class=\"caption caption--about\">\r\n            <h2 class=\"title title--dark\">О проекте</h2>\r\n            <div class=\"subtitle\">Резиденция премиум-класса от «Мармакс»</div>\r\n          </div>\r\n        </div>\r\n      </section>\r\n      <section class=\"location\" id=\"location\">\r\n        <div class=\"container\">\r\n          <div class=\"caption caption--location\">\r\n            <h2 class=\"title title--light title--location\">Расположение</h2>\r\n          </div>\r\n        </div>\r\n      </section>\r\n      <section class=\"options\">\r\n        <div class=\"container\">\r\n          <div class=\"caption caption--option\">\r\n            <h2 class=\"title title--dark\">Опции комфорта</h2>\r\n            <div class=\"subtitle\">Чем наполнить свою жизнь — решать вам</div>\r\n          </div>\r\n        </div>\r\n      </section>\r\n      <section class=\"extra\">\r\n        <div class=\"container\">\r\n          <div class=\"caption\">\r\n            <h2 class=\"title title--light title--extra\">\r\n              Дополнительные опции\r\n            </h2>\r\n          </div>\r\n        </div>\r\n      </section>\r\n      <section class=\"exclusive\">\r\n        <div class=\"container\">\r\n          <div class=\"caption\">\r\n            <h2 class=\"title title--dark\">Эксклюзивные решения</h2>\r\n          </div>\r\n        </div>\r\n      </section>\r\n      <section class=\"gallery\">\r\n        <div class=\"container\">\r\n          <div class=\"caption\">\r\n            <h2 class=\"title title--light\">Галерея</h2>\r\n          </div>\r\n        </div>\r\n      </section>\r\n      <section class=\"sales\">\r\n        <div class=\"container\">\r\n          <section class=\"sales-form\">\r\n            <div class=\"caption\">\r\n              <h2 class=\"title title--dark title--sales\">Офис продаж</h2>\r\n            </div>\r\n          </section>\r\n          <section class=\"sales-map\"></section>\r\n        </div>\r\n      </section>\r\n    </main>\r\n\r\n    <footer class=\"footer\">\r\n      <div class=\"container\"></div>\r\n    </footer>\r\n  </body>\r\n</html>\r\n";
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (code);
+
+/***/ }),
+
+/***/ "../node_modules/html-loader/dist/runtime/getUrl.js":
+/*!**********************************************************!*\
+  !*** ../node_modules/html-loader/dist/runtime/getUrl.js ***!
+  \**********************************************************/
+/***/ ((module) => {
+
+
+
+module.exports = function (url, options) {
+  if (!options) {
+    // eslint-disable-next-line no-param-reassign
+    options = {};
+  }
+
+  if (!url) {
+    return url;
+  } // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+
+
+  url = String(url.__esModule ? url.default : url);
+
+  if (options.hash) {
+    // eslint-disable-next-line no-param-reassign
+    url += options.hash;
+  }
+
+  if (options.maybeNeedQuotes && /[\t\n\f\r "'=<>`]/.test(url)) {
+    return "\"".concat(url, "\"");
+  }
+
+  return url;
+};
+
+/***/ }),
+
+/***/ "./scss/main.scss":
+/*!************************!*\
+  !*** ./scss/main.scss ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./img/logo.png":
+/*!**********************!*\
+  !*** ./img/logo.png ***!
+  \**********************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "img\\logo.e97fdf4b18e39d2d0c53.png";
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		__webpack_require__.p = "";
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"main": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		// no on chunks loaded
+/******/ 		
+/******/ 		// no jsonp function
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!********************!*\
+  !*** ./js/main.js ***!
+  \********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/main.scss */ "./scss/main.scss");
+/* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../index.html */ "./index.html");
+
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
